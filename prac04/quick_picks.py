4e92e4f63814483291aeef6be880a35281073df3
NANGSENGKHAN <62389440+NANGSENGKHAN@users.noreply.github.com>
2020-04-20 13:10:32 +0800

    Initial commit

---
+++ prac04/quick_picks.py
@@ -0,0 +1,34 @@
+"""
+CP1404/CP5632 Practical
+Quick pick program
+"""
+
+"""Author: Nang Seng Khan
+   Date: 20/04/2020 
+"""
+
+import random
+
+number_of_line = 6
+minimum = 1
+maximum = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Invalid")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for n in range(number_of_line):
+            number = random.randint(minimum, maximum)
+            while number in quick_pick:
+                number = random.randint(minimum, maximum)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
