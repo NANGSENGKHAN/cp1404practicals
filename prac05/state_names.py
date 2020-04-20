18c1adcf6a48328f9bff55039607048e99386393
NANGSENGKHAN <62389440+NANGSENGKHAN@users.noreply.github.com>
2020-04-20 13:38:02 +0800

    Initial commit

---
+++ prac05/state_names.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+"""Author: Nang Seng Khan
+   Date: 20/04/2020 
+   link: https://github.com/NANGSENGKHAN/cp1404practicals
+"""
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland",
+                "NSW": "New South Wales",
+                "NT": "Northern Territory",
+                "WA": "Western Australia",
+                "ACT": "Australian Capital Territory",
+                "VIC": "Victoria",
+                "TAS": "Tasmania"}
+
+
+state_code = input("Enter short state: ").strip().upper
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").strip().upper()
+
+
+
